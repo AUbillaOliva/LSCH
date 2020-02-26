90bfec653c6695ae8250565e9ff89d14660d5bba
Alvaro Ubilla Oliva <afubillaoliva@gmail.com>
2020-02-26 02:22:19 -0300

    Added notification channel, code ordering

--- app/src/main/java/cl/afubillaoliva/lsch/MainActivity.java
+++ app/src/main/java/cl/afubillaoliva/lsch/MainActivity.java
@@ -3,8 +3,11 @@ package cl.afubillaoliva.lsch;
 import android.animation.ObjectAnimator;
 import android.animation.StateListAnimator;
 import android.annotation.TargetApi;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,6 +16,7 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +28,7 @@ import android.widget.TextView;
 
 import java.util.Objects;
 
+import cl.afubillaoliva.lsch.activities.DownloadListActivity;
 import cl.afubillaoliva.lsch.activities.FavoriteListActivity;
 import cl.afubillaoliva.lsch.activities.SearchActivity;
 import cl.afubillaoliva.lsch.activities.SettingsActivity;
@@ -33,21 +38,20 @@ import cl.afubillaoliva.lsch.fragments.Expressions;
 import cl.afubillaoliva.lsch.fragments.Themes;
 import cl.afubillaoliva.lsch.utils.SharedPreference;
 
-/*
-TODO'S: GENERIFY getData FUNCTION FOR ALL ACTIVITIES
- */
-
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "API_RESPONSE";
     public static final String FAV = "FAVORITE_RESPONSE";
     public static final String UI = "INTERFACE";
     public static final String HIS = "HISTORY";
+    public static final String REP = "REPORT";
+
+    public static final String CHANNEL_ID = "channel1";
+    public static final String GROUP_KEY = "cl.afubillaoliva.lsch";
 
     public static final int cacheSize = 10 * 1024 * 1024; // 10 MiB
 
     private final Context context = this;
-    private static MainActivity instance;
     private SharedPreference mSharedPreferences;
 
     public Toolbar mToolbar;
@@ -59,22 +63,24 @@ public class MainActivity extends AppCompatActivity {
 
     @TargetApi(Build.VERSION_CODES.M)
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
+        createNotificationChannel();
+
         mSharedPreferences = new SharedPreference(context);
-        if (mSharedPreferences.loadNightModeState()) {
+        if (mSharedPreferences.loadNightModeState())
             setTheme(R.style.AppThemeDark);
-        } else {
+        else
             setTheme(R.style.AppTheme);
-        }
         setContentView(R.layout.activity_main);
 
+        final ViewPager mViewPager = findViewById(R.id.vp_tabs);
+        final StateListAnimator stateListAnimator = new StateListAnimator();
+        final TabLayout mSlidingTabLayout = findViewById(R.id.stl_tabs);
         mToolbar = findViewById(R.id.toolbar);
         appBarLayout = findViewById(R.id.main_appbar_layout);
         appBarLayoutShadow = findViewById(R.id.divider);
-        final ViewPager mViewPager = findViewById(R.id.vp_tabs);
-        final StateListAnimator stateListAnimator = new StateListAnimator();
         toolbarTitle = mToolbar.findViewById(R.id.toolbar_title);
 
         stateListAnimator.addState(new int[0], ObjectAnimator.ofFloat(appBarLayout, "elevation", 2));
@@ -86,13 +92,10 @@ public class MainActivity extends AppCompatActivity {
             mToolbar.setTitleTextAppearance(context, R.style.ToolbarTypefaceLight);
         setSupportActionBar(mToolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
-        mToolbar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(context, SearchActivity.class));
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                finish();
-            }
+        mToolbar.setOnClickListener(v -> {
+            startActivity(new Intent(context, SearchActivity.class));
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
         });
         animateTitleChange(context.getString(R.string.search));
 
@@ -109,35 +112,33 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragment(new Expressions(), "Expresiones de uso cotidiano");
         mViewPager.setAdapter(adapter);
 
-        final TabLayout mSlidingTabLayout = findViewById(R.id.stl_tabs);
         mSlidingTabLayout.setElevation(0);
         if (mSharedPreferences.loadNightModeState())
             mSlidingTabLayout.setBackgroundColor(getResources().getColor(R.color.colorSecondaryDark)); //NIGHT MODE
-        else mSlidingTabLayout.setBackgroundColor(getResources().getColor(R.color.colorSecondaryLight)); //DAY MODE
+        else
+            mSlidingTabLayout.setBackgroundColor(getResources().getColor(R.color.colorSecondaryLight)); //DAY MODE
 
         mSlidingTabLayout.setupWithViewPager(mViewPager);
         mSlidingTabLayout.setUnboundedRipple(true);
 
-        for (int i = 0; i < mSlidingTabLayout.getTabCount(); i++) {
-            TabLayout.Tab tab = mSlidingTabLayout.getTabAt(i);
-            if (tab != null) {
+        for (int i = 0; i < mSlidingTabLayout.getTabCount(); i++){
+            final TabLayout.Tab tab = mSlidingTabLayout.getTabAt(i);
+            if (tab != null)
                 tab.setCustomView(adapter.getTabView(i));
-            }
         }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mSharedPreferences.loadNightModeState()){
+    public boolean onCreateOptionsMenu(Menu menu){
+        if (!mSharedPreferences.loadNightModeState())
             getMenuInflater().inflate(R.menu.menu_main_light, menu);
-        } else {
+        else
             getMenuInflater().inflate(R.menu.menu_main_dark, menu);
-        }
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
             case R.id.action_settings:
                 startActivity(new Intent(context, SettingsActivity.class));
@@ -149,72 +150,90 @@ public class MainActivity extends AppCompatActivity {
                 finish();
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                 break;
+            case R.id.downloads:
+                startActivity(new Intent(context, DownloadListActivity.class));
+                finish();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed(){
         super.onBackPressed();
         finish();
     }
 
-    public static MainActivity getInstance(){
-        return instance;
+    public AppBarLayout getAppBarLayout(){
+        return appBarLayout;
     }
 
-    public AppBarLayout getAppBarLayout(){ return appBarLayout; }
+    public View getAppBarLayoutShadow(){
+        return appBarLayoutShadow;
+    }
 
-    public View getAppBarLayoutShadow(){ return appBarLayoutShadow; }
-
-    private void animateTitleChange(final String newTitle) {
+    private void animateTitleChange(String newTitle){
         final View view = mToolbar.findViewById(R.id.toolbar_title);
 
-        if (view instanceof TextView) {
-            new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        AlphaAnimation fadeOut = new AlphaAnimation(1f, 0f);
-                        fadeOut.setDuration(250);
-                        fadeOut.setAnimationListener(new Animation.AnimationListener() {
-                            @Override
-                            public void onAnimationStart(Animation animation) {
+        if (view instanceof TextView)
+            new android.os.Handler().postDelayed(() -> {
+                    AlphaAnimation fadeOut = new AlphaAnimation(1f, 0f);
+                    fadeOut.setDuration(250);
+                    fadeOut.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation){}
 
-                            }
+                        @Override
+                        public void onAnimationEnd(Animation animation){
+                            toolbarTitle.setText(newTitle);
+                            toolbarTitle.setTextSize(16);
+                            toolbarTitle.setTextColor(Color.GRAY);
 
-                            @Override
-                            public void onAnimationEnd(Animation animation) {
-                                toolbarTitle.setText(newTitle);
-                                toolbarTitle.setTextSize(16);
-                                toolbarTitle.setTextColor(Color.GRAY);
+                            if(mSharedPreferences.loadNightModeState())
+                                toolbarIcon.setImageResource(R.drawable.ic_search_white_24dp);
+                            else
+                                toolbarIcon.setImageResource(R.drawable.ic_search_black_24dp);
 
-                                if(mSharedPreferences.loadNightModeState())
-                                    toolbarIcon.setImageResource(R.drawable.ic_search_white_24dp);
-                                else
-                                    toolbarIcon.setImageResource(R.drawable.ic_search_black_24dp);
-
-                                toolbarIcon.setVisibility(View.VISIBLE);
+                            toolbarIcon.setVisibility(View.VISIBLE);
 
 
-                                llp.gravity = Gravity.START;
-                                toolbarTitle.setLayoutParams(llp);
+                            llp.gravity = Gravity.START;
+                            toolbarTitle.setLayoutParams(llp);
 
-                                AlphaAnimation fadeIn = new AlphaAnimation(0f, 1f);
-                                fadeIn.setDuration(250);
-                                view.startAnimation(fadeIn);
-                            }
+                            AlphaAnimation fadeIn = new AlphaAnimation(0f, 1f);
+                            fadeIn.setDuration(250);
+                            view.startAnimation(fadeIn);
+                        }
 
-                            @Override
-                            public void onAnimationRepeat(Animation animation) {
-
-                            }
-                        });
-                        view.startAnimation(fadeOut);
-                    }
-
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+                        }
+                    });
+                    view.startAnimation(fadeOut);
                 }, 3500
             );
+    }
+
+    private void createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            final CharSequence name = getString(R.string.download_channel_name);
+            final String description = getString(R.string.download_channel_description);
+            final int importance = NotificationManager.IMPORTANCE_LOW;
+            final NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            final NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
         }
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase){
+        super.attachBaseContext(newBase);
+        final Configuration override = new Configuration(newBase.getResources().getConfiguration());
+        override.fontScale = 1.0f;
+        applyOverrideConfiguration(override);
     }
 
 }
