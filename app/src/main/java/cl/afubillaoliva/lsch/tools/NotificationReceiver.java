cc3ae2da59e979d6d73d8a51fa5d460811d567be
Alvaro Ubilla Oliva <afubillaoliva@gmail.com>
2020-03-19 20:24:48 -0300

    Added NotificationReceiver

--- app/src/main/java/cl/afubillaoliva/lsch/tools/NotificationReceiver.java
+++ app/src/main/java/cl/afubillaoliva/lsch/tools/NotificationReceiver.java
@@ -1,4 +1,15 @@
 package cl.afubillaoliva.lsch.tools;
 
-public class NotificationReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class NotificationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        Toast.makeText(context, "Descarga abortada", Toast.LENGTH_SHORT).show();
+        context.stopService(intent);
+    }
 }
