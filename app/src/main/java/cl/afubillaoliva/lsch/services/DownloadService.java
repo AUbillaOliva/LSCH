ed6c5f3818b8271f0f985be0e2770ad03caf776a
Alvaro Ubilla Oliva <afubillaoliva@gmail.com>
2020-04-11 20:38:39 -0400

    Notification action receiver added, String resources added

--- app/src/main/java/cl/afubillaoliva/lsch/services/DownloadService.java
+++ app/src/main/java/cl/afubillaoliva/lsch/services/DownloadService.java
@@ -1,10 +1,10 @@
 package cl.afubillaoliva.lsch.services;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.ResultReceiver;
-import android.util.Log;
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -16,66 +16,98 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.text.Normalizer;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.JobIntentService;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import cl.afubillaoliva.lsch.MainActivity;
 import cl.afubillaoliva.lsch.MainApplication;
 import cl.afubillaoliva.lsch.R;
 import cl.afubillaoliva.lsch.models.Word;
+import cl.afubillaoliva.lsch.tools.NotificationReceiver;
 import cl.afubillaoliva.lsch.utils.SharedPreference;
 
-public class DownloadService extends JobIntentService {
+import android.app.IntentService;
 
+import androidx.annotation.Nullable;
+
+public class DownloadService extends IntentService implements NotificationReceiver.Receiver {
 
     public final static int SERVICE_ID = 371492;
-    private static int fileLength = 0, maxProgress = 0;
-    private static int i = 0;
+    private static int fileLength = 0;
+    private int i = 0, maxProgress = 1;
     private NotificationManagerCompat notificationManagerCompat;
     private NotificationCompat.Builder notification;
-    private SharedPreference mSharedPreferences;
+    private Intent service;
+    private PendingIntent pendingIntent;
+    private String list;
     private Context context;
 
-    public static void enqueueWork(Context context, Intent intent){
-        enqueueWork(context, DownloadService.class, SERVICE_ID, intent);
+    public DownloadService(){
+        super(DownloadService.class.getSimpleName());
+    }
+
+    public DownloadService(String list, Context context){
+        super(DownloadService.class.getSimpleName());
+        this.list = list;
+        this.context = context;
     }
 
     @Override
-    public void onCreate() {
+    public void onCreate(){
         super.onCreate();
-        Log.e(MainActivity.DOWN, "onCreate");
 
         context = MainApplication.getContext();
 
-        mSharedPreferences = new SharedPreference(context);
+        final SharedPreference mSharedPreferences = new SharedPreference(context);
         notificationManagerCompat = NotificationManagerCompat.from(context);
 
+        final NotificationReceiver notificationReceiver = new NotificationReceiver();
+        notificationReceiver.setReceiver(this);
+
+        service = new Intent(context, NotificationReceiver.class);
+        service.putExtra("list", list);
+        pendingIntent = PendingIntent.getBroadcast(context, 0, service, PendingIntent.FLAG_UPDATE_CURRENT);
+
         notification = new NotificationCompat.Builder(context, MainActivity.CHANNEL_ID)
-                .setContentTitle("Descarga")
+                .setContentTitle(context.getResources().getString(R.string.download))
                 .setShowWhen(true)
-                .setContentText("Descarga en progreso...")
+                .setContentText(context.getResources().getString(R.string.download_in_progress))
+                .addAction(R.drawable.ic_close_24dp, context.getResources().getString(R.string.negative_dialog_button), pendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_LOW);
         if(mSharedPreferences.loadNightModeState())
             notification.setSmallIcon(R.drawable.ic_app_icon_light);
         else
             notification.setSmallIcon(R.drawable.ic_app_icon_dark);
 
-        notificationManagerCompat.notify(SERVICE_ID, notification.build());
+        startForeground(SERVICE_ID, notification.build());
+    }
+
+    public static String stripAccents(String s){
+        s = s.replaceAll("/", "");
+        s = Normalizer.normalize(s, Normalizer.Form.NFD);
+        s = s.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+        return s;
     }
 
     @Override
-    protected void onHandleWork(@NonNull Intent intent) {
-
+    protected void onHandleIntent(@Nullable Intent intent){
+        assert intent != null;
         final Bundle bundle = intent.getExtras();
         assert bundle != null;
         final Word word = (Word) bundle.getSerializable("data");
 
-        maxProgress = intent.getIntExtra("maxProgress", 0);
+        list = bundle.getString("list");
+        service.putExtra("list", list);
+        pendingIntent = PendingIntent.getBroadcast(context, 0, service, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        maxProgress = intent.getIntExtra("maxProgress", 1);
 
         final ResultReceiver receiver = intent.getParcelableExtra("receiver");
         assert word != null;
         final File file = new File(getExternalFilesDir(null) + File.separator + stripAccents(word.getTitle()) + ".mp4");
+
+        notification.setContentText(i + " " + context.getResources().getString(R.string.of) + " " + maxProgress + " (" + (i*100)/ maxProgress + "% " + context.getResources().getString(R.string.completed) +")");
+        notificationManagerCompat.notify(SERVICE_ID, notification.build());
+
         if(!file.exists()){
             try {
                 final URL url = new URL(word.getImages().get(0));
@@ -91,9 +123,7 @@ public class DownloadService extends JobIntentService {
                 final byte[] data = new byte[1024];
                 long total = 0;
                 int count;
-                notification.setContentText(i + " de " + maxProgress + " (" + (i*100)/maxProgress + "% Completado)");
-                notificationManagerCompat.notify(SERVICE_ID, notification.build());
-                while ((count = input.read(data)) != -1) {
+                while ((count = input.read(data)) != -1){
                     total += count;
                     final Bundle resultData = new Bundle();
                     resultData.putInt("progress" ,(int) (total * 100 / fileLength));
@@ -106,7 +136,7 @@ public class DownloadService extends JobIntentService {
                 output.close();
                 input.close();
 
-            } catch (IOException e) {
+            } catch (IOException e){
                 e.printStackTrace();
             }
 
@@ -120,38 +150,14 @@ public class DownloadService extends JobIntentService {
         }
     }
 
-    public static String stripAccents(String s){
-        s = s.replaceAll("/", "");
-        s = Normalizer.normalize(s, Normalizer.Form.NFD);
-        s = s.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
-        return s;
-    }
-
     @Override
-    public void onDestroy() {
+    public void onDestroy(){
         super.onDestroy();
-        Log.e(MainActivity.DOWN, "onDestroy");
-        maxProgress = 0;
         i = 0;
+        maxProgress = 1;
         notificationManagerCompat.cancel(SERVICE_ID);
-        notification = new NotificationCompat.Builder(context, MainActivity.CHANNEL_ID)
-                .setShowWhen(false)
-                .setContentTitle("Descarga completada")
-                .setAutoCancel(true)
-                .setShowWhen(true)
-                .setPriority(NotificationCompat.PRIORITY_LOW);
-        if(mSharedPreferences.loadNightModeState())
-            notification.setSmallIcon(R.drawable.ic_app_icon_light);
-        else
-            notification.setSmallIcon(R.drawable.ic_app_icon_dark);
-
-        notificationManagerCompat.notify(SERVICE_ID, notification.build());
-
     }
 
     @Override
-    public boolean onStopCurrentWork() {
-        Log.e(MainActivity.DOWN, "onStopCurrentWork");
-        return super.onStopCurrentWork();
-    }
+    public void onReceive(Context context, Intent intent){}
 }
