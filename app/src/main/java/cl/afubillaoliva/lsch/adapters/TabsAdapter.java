7d011046ff2705e9abbe3a27dcc391d4ae536837
Alvaro Ubilla Oliva <afubillaoliva@gmail.com>
2020-04-22 21:40:48 -0400

    Adapter extended to FragmentStateAdapter

--- app/src/main/java/cl/afubillaoliva/lsch/adapters/TabsAdapter.java
+++ app/src/main/java/cl/afubillaoliva/lsch/adapters/TabsAdapter.java
@@ -6,8 +6,7 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
@@ -15,18 +14,35 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 import cl.afubillaoliva.lsch.R;
 
-public class TabsAdapter extends FragmentStatePagerAdapter {
+public class TabsAdapter extends FragmentStateAdapter {
 
     private Context context;
 
     private final List<Fragment> mFragmentList = new ArrayList<>();
     private final List<String> mFragmentTitleList = new ArrayList<>();
 
-    public TabsAdapter(FragmentManager fm, Context context){
-        super(fm);
+    public TabsAdapter(FragmentActivity fragment, Context context){
+        super(fragment);
         this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position){
+        return mFragmentList.get(position);
+    }
+
+    @Nullable
+    public CharSequence getPageTitle(int position){
+        return mFragmentTitleList.get(position);
+    }
+
+    @Override
+    public int getItemCount(){
+        return mFragmentList.size();
     }
 
     public View getTabView(int position){
@@ -36,24 +52,9 @@ public class TabsAdapter extends FragmentStatePagerAdapter {
         return v;
     }
 
-    @NonNull
-    @Override
-    public Fragment getItem(int position){
-        return mFragmentList.get(position);
-    }
     public void addFragment(Fragment fragment, String title){
         mFragmentList.add(fragment);
         mFragmentTitleList.add(title);
     }
 
-    @Nullable
-    @Override
-    public CharSequence getPageTitle(int position){
-        return mFragmentTitleList.get(position);
-    }
-
-    @Override
-    public int getCount(){
-        return mFragmentList.size();
-    }
 }
